8156a8f4ec898b54bd6a2ceb9e618c78eddcc490
Joel Klabo <joelklabo@gmail.com>
2015-12-09 11:33:48 -0800

    Irrelevant route

--- server.py
+++ server.py
@@ -14,10 +14,6 @@ app = Flask(__name__)
 #wallet = Wallet()
 #payment = Payment(app, wallet)
 
-@app.route('/risk/<int:risk_amount>')
-def risk(risk_amount):
-  return 'you risked {:d}'.format(risk_amount)
-  
 if __name__ == '__main__':
   app.debug = True
   app.run(host='0.0.0.0', port=PORT)
