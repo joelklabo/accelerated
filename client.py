9a41df7ee2867887dd48e8b4dab252bf75d498d0
Joel Klabo <joelklabo@gmail.com>
2015-12-10 17:37:46 +0000

    adding get element methods

---
+++ client.py
@@ -0,0 +1,48 @@
+import json
+import time
+import subprocess
+
+# Import from 21 developer library
+import two1.commands.status
+from two1.lib.server import rest_client
+from two1.commands.config import TWO1_HOST
+from two1.commands.config import Config
+from two1.lib.wallet import Wallet
+from two1.lib.bitrequests import BitTransferRequests
+
+SERVER_URL = 'http://localhost:5003/fastfind'
+
+# Set up wallet proxy
+wallet = Wallet()
+
+# Read local user account data
+config = Config()
+username = Config().username
+
+# Initialize rest client to communicate with 21.co API
+client = rest_client.TwentyOneRestClient(TWO1_HOST, config.machine_auth, config.username)
+
+# Set up bitrequests client using BriTransfer payment method for 402 requests
+requests = BitTransferRequests(wallet, username)
+
+# Check the spot price for our API call
+response = requests.get_402_info(url=SERVER_URL)
+endpoint_info = dict(response)
+price = int(endpoint_info['price'])
+print(endpoint_info)
+
+def get_element(arr, prop, val):
+  for elem in arr:
+    if elem[prop] == val:
+      return val
+    time.sleep(1)
+
+# fast bitcoin aware api NOx
+def fast_get_element(arr, prop, val):
+  body = {
+    'array': json.dumps(arr),
+    'property': prop,
+    'value': val
+  }
+  res = requests.post(url=SERVER_URL, data=body)
+  return json.loads(res.text)['elem']
